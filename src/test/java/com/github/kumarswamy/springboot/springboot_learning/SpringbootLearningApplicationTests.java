bddd65b45efaa5ba58afc3906786a464f557b75d
kumar swamy <kumar.swamy5907@gmail.com>
2025-09-27 22:55:22 +0530

    first spring boot commit

---
+++ src/test/java/com/github/kumarswamy/springboot/springboot_learning/SpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kumarswamy.springboot.springboot_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
